4411ce471ca41aaa93275d1b22abaa91f9412598
Mykolas Paulauskas <33966587+Rapsas@users.noreply.github.com>
2024-03-11 22:54:56 +0200

    Revert "an oopsie"
    This reverts commit 813c9670cbc382278748e330b6d6eeb0daaa102c.

--- app/src/main/java/com/example/praktikos_darbas_1/MainActivity.java
+++ app/src/main/java/com/example/praktikos_darbas_1/MainActivity.java
@@ -14,7 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    //WHAT EVER YOU DONT COMMIT THIS COMMENT
+
     private Button changeTextButton;
     private TextView changeTextView;
     private  Button changeColorButton;
